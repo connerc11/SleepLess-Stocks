60fb79c07fd1bff746b508da466c7cbf38e96bf5
Conner Cochrane <cccochrane1@gmail.com>
2025-06-29 20:17:22 -0400

    added

---
+++ SleeplessStocks/FrontEnd/src/API_CONFIG.js
@@ -0,0 +1,8 @@
+// frontend/src/API_CONFIG.js
+const API_CONFIG = {
+  FMP_API_KEY: 'xpdz5zYVIUGwIFkqJnPaZStwa1DU7UHo'}
+  ;
+
+  export const API_BASE_URL = "https://sleepless-stocks.onrender.com";
+
+export default API_CONFIG;
